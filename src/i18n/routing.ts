dfd162b9cabab1540ecc979756934ad278fee523
kanekicode <kanekicode5@gmail.com>
2026-01-30 14:45:02 -0500

    Spanish translation of /messages

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,8 +1,9 @@
 import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
+import { locales } from './config';
 
 export const routing = defineRouting({
-  locales: ['en', 'fr'],
+  locales,
   defaultLocale: 'en',
   localePrefix: 'as-needed',
   localeDetection: false,
